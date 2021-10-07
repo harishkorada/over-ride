1e442b58776d08cd6534e7d968deff0151ec812e
harishkorada <koradaharish.king@gmail.com>
2021-10-07 22:47:12 +0530

    ravi

---
+++ src/org/col/Arts.java
@@ -0,0 +1,45 @@
+package org.col;
+
+import org.sample.Education;
+
+public class Arts  extends Education {
+	
+	@Override
+	public void ug() {
+		System.out.println("in arts ug also viable");
+		super.ug();
+	
+	}
+	@Override
+	public void pg() {
+		System.out.println("in arts pg also aviable");
+		super.pg();
+	}
+	private void be() {
+		System.out.println("in arts college this course aviable");
+
+	}
+	private void bsc() {
+		System.out.println("in arts bsc course aviable");
+
+	}
+	public static void main(String[] args) {
+		Arts p =new Arts();
+		p.be();
+		p.bsc();
+		p.pg();
+		p.ug();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	}
+
+
---
+++ src/org/sample/Education.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Education {
+	public void ug() {
+System.out.println("in this college ug coure is aviable");
+
+	}
+	public void pg() {
+		System.out.println("in this college pg course is aviable");
+
+	}
+	
+
+}
